f82bf33cf6539fb4a817bd69c601dee943f15aaa
Carlos Gil Sabrido <carliros-gs@hotmail.com>
2019-01-24 12:03:27 +0100

    Cambios de configuracion de proyecto

---
+++ PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/PracticaDadApplication.java
@@ -0,0 +1,14 @@
+package es.urjc.etsii.dad.Practica_DAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaDadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaDadApplication.class, args);
+	}
+
+}
+
---
+++ PracticaDAD/src/test/java/es/urjc/etsii/dad/Practica_DAD/PracticaDadApplicationTests.java
@@ -0,0 +1,17 @@
+package es.urjc.etsii.dad.Practica_DAD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticaDadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
