560f3ac12ec15455b5fe2d0d97815624830db100
CristianPS <cristianvk97@gmail.com>
2019-02-07 12:47:26 +0100

    Creado ComercioRepository

---
+++ PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/ComercioRepository.java
@@ -0,0 +1,8 @@
+package es.urjc.etsii.dad.Practica_DAD;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComercioRepository extends JpaRepository<Comercio, Long> {
+
+	
+}
