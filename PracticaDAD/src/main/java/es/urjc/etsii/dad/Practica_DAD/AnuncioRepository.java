32f347b885dc45170a5bfe2f2d5b280ad8201bfe
sito <sito_diaz@hotmail.es>
2019-02-12 18:38:24 +0100

    Modificacion en AnuncioRepository

--- PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/AnuncioRepository.java
+++ PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/AnuncioRepository.java
@@ -1,7 +1,12 @@
 package es.urjc.etsii.dad.Practica_DAD;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnuncioRepository extends JpaRepository<Anuncio, Long>{
-
+	
+	List<Anuncio> findByTitle(String title);
+	
+	Anuncio getByTitle(String title);
 }
