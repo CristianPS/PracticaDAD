53a33084ad9cedb773d518120e38aeb0a4edd0a9
Carlos Gil Sabrido <carlitos-gs@hotmail.com>
2019-02-13 22:42:36 +0100

    arreglos en perfil

---
+++ PracticaDAD/src/main/resources/static/scripts/Editar.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+
+function editar() {
+	document.getElementById("pass").classList.remove("invisible");
+	document.getElementById("newPass").classList.remove("invisible");
+	ocument.getElementById("newPass").classList.add("hola");
+
+}
--- PracticaDAD/src/main/resources/static/scripts/loginscript.js
+++ PracticaDAD/src/main/resources/static/scripts/loginscript.js
@@ -31,3 +31,15 @@ function registro()
 	document.getElementById("form").action = "registro.html"
 	document.getElementById("form").method = "";
 }
+
+function editarA() {
+	document.getElementById("pass").classList.remove("invisible");
+	document.getElementById("newPass").classList.remove("invisible");
+	document.getElementById('nombre').disabled = false;
+	document.getElementById('apellidos').disabled = false;
+	document.getElementById('gender').disabled = false;
+	document.getElementById('ciudad').disabled = false;
+	document.getElementById('fecha').disabled = false;
+	document.getElementById('correo').disabled = false;
+
+}
