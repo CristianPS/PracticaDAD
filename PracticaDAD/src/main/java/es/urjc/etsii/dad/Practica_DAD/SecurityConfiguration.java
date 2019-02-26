b0ea3b8117d38bc1401ed9017561d38f83a6a99b
CristianPS <cristianvk97@gmail.com>
2019-02-26 14:04:52 +0100

    Ya coge el CSS.

--- PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/SecurityConfiguration.java
+++ PracticaDAD/src/main/java/es/urjc/etsii/dad/Practica_DAD/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package es.urjc.etsii.dad.Practica_DAD;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,10 +18,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 		
 		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/images/**", "/styles/**", "/scripts/**").permitAll();
 		http.authorizeRequests().antMatchers("/login").permitAll();
 		http.authorizeRequests().antMatchers("/registro").permitAll();
+		
 		http.authorizeRequests().anyRequest().authenticated();
 		
+		http.formLogin().loginPage("/login");
+		http.formLogin().usernameParameter("name");
+		http.formLogin().passwordParameter("password");
+		http.formLogin().defaultSuccessUrl("/inicioUsuario");
 		
+		http.csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("pass").roles("USER");
 	}
 }
